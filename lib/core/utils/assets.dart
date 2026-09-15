d36430ac892bd76c4a296932f9e045a63781b04c
mohammed <mohamedtamernasar7779@gmail.com>
2026-09-15 04:48:23 +0300

    create a customListViewItem

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,5 @@
 class AssetsImage {
   static const logo = 'assets/images/BOOKLY.png';
+  static const testImage1 = 'assets/images/test_image1.jpg';
+  static const testImage2 = 'assets/images/test_image2.jpg';
 }
---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .32,
+      child: AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            image: DecorationImage(image: AssetImage(AssetsImage.testImage1)),
+          ),
+        ),
+      ),
+    );
+  }
+}
