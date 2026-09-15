40bdae1bd79ae4a329df605193abd6968d0809e8
mohammed <mohamedtamernasar7779@gmail.com>
2026-09-15 18:50:51 +0300

    BestSellerListViewItem partOne

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view.dart';
@@ -18,7 +19,38 @@ class HomeViewBody extends StatelessWidget {
           padding: EdgeInsets.only(left: 24, top: 50),
           child: Text('Best Seller', style: Styles.titleMedium),
         ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: BestSellerListViewItem(),
+        ),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: AssetImage(AssetsImage.testImage1),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
