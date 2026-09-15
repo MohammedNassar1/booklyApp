51a5ae79c460c2dcf4628c5dd8e8483db309055f
mohammed <mohamedtamernasar7779@gmail.com>
2026-09-15 03:40:47 +0300

    create a home view and navigation

---
+++ lib/features/splash/presentation/views/widgets/slide_transition_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class slideTransitionText extends StatelessWidget {
+  const slideTransitionText({
+    super.key,
+    required this.slidinganimation,
+  });
+
+  final Animation<Offset> slidinganimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidinganimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidinganimation,
+          child: Text('Read free books'),
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,15 +1,62 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/slide_transition_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/instance_manager.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinganimation;
+  @override
+  void initState() {
+    super.initState();
+    slidingAnimation();
+    navigationToHome();
+  }
+
+ 
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssitsImage.logo)],
+      children: [
+        Image.asset(AssitsImage.logo),
+        Center(child: slideTransitionText(slidinganimation: slidinganimation)),
+      ],
     );
+  }
+
+  void slidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slidinganimation = Tween<Offset>(
+      begin: Offset(0, 7),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+   void navigationToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(), transition: Transition.fadeIn);
+    });
   }
 }
