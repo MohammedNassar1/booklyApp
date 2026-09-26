ee6ecf9c5bd0eebf6ae8bf8c4f6b24e831be272b
mohammed <mohamedtamernasar7779@gmail.com>
2026-09-26 09:42:30 +0300

    create a return types of mehods of HomeRepo

--- lib/features/details/data/Repo/home_repo.dart
+++ lib/features/details/data/Repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/details/data/models/bookmodel/bookmodel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBook();
-  fetchFeaturedBooks();
+  Future<Either<Failures, List<Bookmodel>>> fetchBestSellerBook();
+  Future<Either<Failures, List<Bookmodel>>> fetchFeaturedBooks();
 }
